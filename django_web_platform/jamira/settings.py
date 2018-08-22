3787dea0f3d107f104bcb2f3924dbde4403ecad8
Icaro Maia Santos de Castro <icaromscastro@gmail.com>
2018-08-22 12:07:16 -0300

    modelos atualizados

--- django_web_platform/jamira/settings.py
+++ django_web_platform/jamira/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'django.contrib.admin',
+    'jamira.apps.JamiraPlatformConfig',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
@@ -118,3 +119,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
