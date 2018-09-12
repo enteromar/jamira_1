4eeffa32f3931b2a9236bdb97c077f205d31bc73
Ícaro Castro <icaromscastro@gmail.com>
2018-09-12 10:53:35 -0300

    ferramenta selecionada é executada no pipeline

--- django_web_platform_v2/enteromics/core/pipelines.py
+++ django_web_platform_v2/enteromics/core/pipelines.py
@@ -8,15 +8,18 @@ class AutomatizedTool(object):
 
     def start(self):
         print ("Selected ", self.tool ," for genome analysis...")
-        if(self.tool == 'Virulence Finder'):
+
+        if(self.tool == 'VirulenceFinder'):
             print ('\nStarting Virulence Finder Data Analysis...\n\n\n')
-            command = "perl /home/icaro/virulence_finder/virulencefinder.pl -d /home/icaro/virulence_finder/database/ -i " + self.filepath + " -o out_virulence -s virulence_ent -k 85.00"
-            print(command)
-            os.system(command)
-            print ('\nVirulence Finder Data Analysis finished...\n\n\n')
+            #command = "perl /home/icaro/virulence_finder/virulencefinder.pl -d /home/icaro/virulence_finder/database/ -i " + self.filepath + " -o analysis_results/out_virulence -s virulence_ent -k 85.00"
+            #print(command)
+            #os.system(command)
+            #print ('\nVirulence Finder Data Analysis finished...\n\n\n')
         if(self.tool == 'RGI'):
             print ('\nStarting RGI Data Analysis...\n\n\n')
-            command = "rgi main -i "+ self.filepath + " -o resistance_out/ -t contig -n 4 -a BLAST"
-            print(command)
-            os.system(command)
-            print ('\nRGI Data Analysis finished...\n\n\n')
+            #command = "rgi main -i "+ self.filepath + " -o results/resistance_out/ -t contig -n 4 -a BLAST"
+            #print(command)
+            #os.system(command)
+            #print ('\nRGI Data Analysis finished...\n\n\n')
+        if(self.tool == 'PhySpy'):
+            print ('\nStarting Physpy Data Analysis...\n\n\n')
--- django_web_platform_v2/enteromics/core/views.py
+++ django_web_platform_v2/enteromics/core/views.py
@@ -30,11 +30,13 @@ def upload(request):
 
             #Get selected_tool from form
             selected_tool = form.cleaned_data['tools']
+            selected_tool = selected_tool[0].tool_name
+            print(selected_tool)
             #get absolute genome file path
             filepath=fs.path(filename)
 
             #call Analysis
-            job = AutomatizedTool("Virulence Finder",filepath)
+            job = AutomatizedTool(selected_tool,filepath)
             job.start()
 
 
