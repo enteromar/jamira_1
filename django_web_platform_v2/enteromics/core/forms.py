cc40444c4d03334bc7b7ba7dd219a13821289474
Icaro Maia Santos de Castro <icaromscastro@gmail.com>
2018-09-19 09:57:59 -0300

    form upload integration with bootstrap

--- django_web_platform_v2/enteromics/core/forms.py
+++ django_web_platform_v2/enteromics/core/forms.py
@@ -4,6 +4,7 @@ from core.models import Tool,Genomic_file,Document
 class ToolRequestForm(forms.Form):
     #name = forms.CharField(max_length=100)
     tools = forms.ModelMultipleChoiceField(queryset=Tool.objects.all())
+    #genomic_file = forms.FileField()
     # class Meta:
     #     model = Genomic_file
     #     fields = ('isolation_source','host','upload' )
